24c20c53fdf7e74deb7d144943162193b5f1cb28
loimh <765783376@qq.com>
2020-12-08 18:26:56 +0800

    * 去掉无用代码

--- mysqlMasterSlave/src/main/java/com/example/demo/utils/ExceptionControllerAdvice.java
+++ mysqlMasterSlave/src/main/java/com/example/demo/utils/ExceptionControllerAdvice.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+/**
+ * 全局异常处理类
+ */
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
